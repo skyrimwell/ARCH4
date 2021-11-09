655febabd2370c6015608abceec3d7a097de892a
skyrimwell <51721698+skyrimwell@users.noreply.github.com>
2021-11-09 13:58:16 +0300

    Initial commit

---
+++ src/main/java/com/example/zdarovamessage/zdarovamessage.java
@@ -0,0 +1,12 @@
+package com.example.zdarovamessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class zdarovamessage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(zdarovamessage.class, args);
+	}
+}
